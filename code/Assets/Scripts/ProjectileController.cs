f74cbd47a5bd289e1d2715091edab53b0b7005e7
Short Course 022 <sc022>
2022-04-28 15:42:08 +0100

    start of fire scene

--- code/Assets/Scripts/ProjectileController.cs
+++ code/Assets/Scripts/ProjectileController.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TerrainController : MonoBehaviour
+public class ProjectileController : MonoBehaviour
 {
 
     public float speed = 2f;
     public ArrayList objects;
     public GameObject player;
-    public GameObject Particle;
+    public GameObject Rock;
 
     public float offset = 15f;
 
@@ -34,40 +34,41 @@ public class TerrainController : MonoBehaviour
 
     void SpawnRandomBox()
     {
-        GameObject g = Instantiate(Particle);
+        GameObject g = Instantiate(Rock);
         Vector3 playerPos = player.transform.position;
         Vector3 pos = new Vector3(playerPos.x + offset, Random.Range(1, 5), 0);
         g.transform.Translate(pos);
         g.GetComponent<ObjectScript>().terrainController = this.gameObject;
         objects.Add(g.GetComponent<ObjectScript>());
-        
+
     }
 
-    float relativeXPosition(Vector3 v3) {
+    float relativeXPosition(Vector3 v3)
+    {
         return player.transform.position.x - v3.x;
-    }   
+    }
 
     void moveObjects()
     {
         float movement = speed * Time.deltaTime;
         foreach (ObjectScript obj in objects)
         {
-                obj.move(new Vector3(-movement, 0, 0));
+            obj.move(new Vector3(-movement, 0, 0));
         }
     }
 
     void checkObjects()
     {
         ArrayList dels = new ArrayList();
-        foreach(ObjectScript obj in objects)
+        foreach (ObjectScript obj in objects)
         {
-            if(deletionDistance - obj.gameObject.transform.position.x <= 0)
+            if (deletionDistance - obj.gameObject.transform.position.x <= 0)
             {
                 dels.Add(obj);
             }
         }
 
-        foreach(ObjectScript obj in dels)
+        foreach (ObjectScript obj in dels)
         {
             objects.Remove(obj);
             Destroy(obj.gameObject);
