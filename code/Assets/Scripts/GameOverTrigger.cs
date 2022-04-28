4353209b712f8486174dfc867e83e273a5b11e47
siltn <siltn.depuydt@student.howest.be>
2022-04-29 00:36:50 +0200

    links

--- code/Assets/Scripts/GameOverTrigger.cs
+++ code/Assets/Scripts/GameOverTrigger.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class GameOverTrigger : MonoBehaviour
 {
      void OnTriggerEnter2D(Collider2D other) {
-        SceneManager.LoadScene(3);
+        SceneManager.LoadScene(2);
 
     }
 }
